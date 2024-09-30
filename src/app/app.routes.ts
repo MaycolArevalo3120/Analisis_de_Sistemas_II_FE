a4553b40a50f55a1a4dc21c7eb3d445d938cd649
Arevalo Borrayo <amjareval@sat.gob.gt>
2024-09-30 09:44:52 -0600

    Se versiona un mínimo cambio para el tema de probarlo en otro equipo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
         children: [
             {
                 path: 'proyectos',
-                title: 'Asignación de proyectos',
+                title: 'Asignación de proyectos s',
                 loadComponent: () => import('./dashboard/pages/proyectos/proyectos.component'),
             },
             {
